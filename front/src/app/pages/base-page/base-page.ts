0940ec2f309b25f7fed4e102b77188755a77b312
Ulysses Gomes <ugomes089@gmail.com>
2026-09-20 14:06:19 -0300

    [task-0003] - Adicionando Tailwind styles; Desenvolvimento do component Footer.

--- front/src/app/pages/base-page/base-page.ts
+++ front/src/app/pages/base-page/base-page.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { ButtonModule } from 'primeng/button';
 
+import { Footer } from '../../core/components/footer/footer';
+
 @Component({
   selector: 'app-base-page',
-  imports: [ButtonModule],
+  imports: [RouterOutlet, ButtonModule, Footer],
   templateUrl: './base-page.html',
   styleUrl: './base-page.scss',
 })
