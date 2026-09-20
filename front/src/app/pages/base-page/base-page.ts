ed888dc43440a3b497bc8de39666a3e6bcc8068f
Ulysses Gomes <ugomes089@gmail.com>
2026-09-20 12:47:36 -0300

    [task-0002] - Adicionando framework Primeng ao projeto.

--- front/src/app/pages/base-page/base-page.ts
+++ front/src/app/pages/base-page/base-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-base-page',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './base-page.html',
   styleUrl: './base-page.scss',
 })
