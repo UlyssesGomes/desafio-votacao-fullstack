86de11464a3f0f4f35775deb5666d846747e3112
Ulysses Gomes <ugomes089@gmail.com>
2026-09-20 21:53:35 -0300

    [hotfix-backend] - correção da validação do tempo mínimo da sessão; Adição de novos campos no detalhamento da pauta.

--- back/src/main/java/com/votacao/app/dto/AbrirSessaoDTO.java
+++ back/src/main/java/com/votacao/app/dto/AbrirSessaoDTO.java
@@ -1,9 +1,11 @@
 package com.votacao.app.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Positive;
 
 public record AbrirSessaoDTO(
         @Positive(message = "{sessao.duracao}")
+        @Min(value = 60, message = "{sessao.duracao}")
         Long duracao
 ) {
 }
--- back/src/main/java/com/votacao/app/dto/PautaDetalheRespostaDTO.java
+++ back/src/main/java/com/votacao/app/dto/PautaDetalheRespostaDTO.java
@@ -2,12 +2,16 @@ package com.votacao.app.dto;
 
 import com.votacao.app.model.Pauta;
 
+import java.time.Instant;
 import java.util.List;
 
 public record PautaDetalheRespostaDTO(
         Long id,
         String titulo,
         String descricao,
+        Instant dataAbertura,
+        Instant dataEncerramento,
+        boolean sessaoFinalizada,
         List<VotoDetalheDTO> votos
 ) {
     public PautaDetalheRespostaDTO(Pauta pauta) {
@@ -15,6 +19,7 @@ public record PautaDetalheRespostaDTO(
         if(pauta.getVotos() != null && pauta.getVotos().size() > 0) {
             votoDetalheDTOList = pauta.getVotos().stream().map(voto -> new VotoDetalheDTO(voto)).toList();
         }
-        this(pauta.getId(), pauta.getTitulo(), pauta.getDescricao(), votoDetalheDTOList);
+        this(pauta.getId(), pauta.getTitulo(), pauta.getDescricao(), pauta.getDataAbertura(),
+                pauta.getDataEncerramento(), pauta.isSessaoFinalizada(), votoDetalheDTOList);
     }
 }
