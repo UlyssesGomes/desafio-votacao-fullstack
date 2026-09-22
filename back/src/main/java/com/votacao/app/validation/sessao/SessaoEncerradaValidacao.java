21770111df7aabf1ad272d9e2b009369be0cc2db
Ulysses Gomes <ugomes089@gmail.com>
2026-09-21 21:48:38 -0300

    [task-0002] - adicionando log de error e de info nas validações e no PautaService.

--- back/src/main/java/com/votacao/app/validation/sessao/SessaoEncerradaValidacao.java
+++ back/src/main/java/com/votacao/app/validation/sessao/SessaoEncerradaValidacao.java
@@ -2,16 +2,20 @@ package com.votacao.app.validation.sessao;
 
 import com.votacao.app.exceptions.SessaoException;
 import com.votacao.app.model.Pauta;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 
 @Component
+@Slf4j
 public class SessaoEncerradaValidacao implements SessaoValidacao {
     @Override
     public void validar(Pauta pauta) {
         if(pauta.getDataEncerramento()  != null && Instant.now().isAfter(pauta.getDataEncerramento())) {
-            throw new SessaoException("A sessão já foi encerrada, não pode ser aberta novamente.");
+            String errorMessage = "A sessão já foi encerrada, não pode ser aberta novamente.";
+            log.error(errorMessage);
+            throw new SessaoException(errorMessage);
         }
     }
 }
